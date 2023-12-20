930295ca3684d77d905535aff445c045d51dc375
Luciano <lucianobonilla27@gmail.com>
2023-12-20 09:02:33 -0300

    clase Producto

---
+++ js/tarea.js
@@ -0,0 +1,13 @@
+let productos = [];
+
+class Producto {
+    constructor(id, nombre, descripcion, precio) {
+      this.id = id;
+      this.nombre = nombre;
+      this.descripcion = descripcion;
+      this.precio = precio;
+    }
+  }
+  
+
+  
